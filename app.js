1a14bee944eccc9d633616b7ba505a7699c26084
Jakob Salej <jakob.salej@gmail.com>
2017-04-28 00:31:16 +0200

    Changes.

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.get('/webhook', function(req, res) {
 
 // get messages from users
 app.post('/webhook', function (req, res) {
-  //console.log('Getting post request!');
+  console.log('Getting post request!');
   var data = req.body;
-  //console.log('This is req data:', data);
+  console.log('This is req data:', data);
 
   // Make sure this is a page subscription
   if (data.object === 'page') {
@@ -63,7 +63,7 @@ app.post('/webhook', function (req, res) {
 
 // launch app!
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+  console.log('Example app listening on port');
 });
 
 
