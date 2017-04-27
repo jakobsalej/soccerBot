da4018debe067f1c552c96089c258699888299c7
Jakob Salej <jakob.salej@gmail.com>
2017-04-27 23:10:23 +0200

    Fixing start.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const verify_token = "this_is_my_token";
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+
+// variables
+const verify_token = "this_is_my_token";
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 
 // messenger part
 app.get('/webhook', function(req, res) {
@@ -53,8 +60,8 @@ app.post('/webhook', function (req, res) {
 
 
 // launch app!
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
 
 
