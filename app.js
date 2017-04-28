d5150bf463b193d039aac5d864eefdad94e430ff
Jakob Salej <jakob.salej@gmail.com>
2017-04-28 14:28:59 +0200

    Changes.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   console.log('Requesting root...');
